1c87345c2135fcce1475107026d9f9e97f09072f
mertcd <mertcandenli3@gmail.com>
2023-06-18 19:43:04 +0300

    new things

--- live_capture.py
+++ live_capture.py
@@ -1,32 +1,35 @@
 import pyshark
 
-def print_live_packets(interface,mac ,gat):
-    capture = pyshark.LiveCapture(interface,"arp")
+
+def print_live_packets(interface, mac, gat):
+    capture = pyshark.LiveCapture(interface, "arp")
 
     # Set the packet count limit, if desired
     # capture.sniff(packet_count=10)
 
     for packet in capture.sniff_continuously():
-        # Print the packet details
-        if ((packet.src.proto_ipv4 == " + gat + ") and (packet.opcode == 2)) and not (packet.src.hw_mac == " + mac + "):
-            print("Arp poisoning at "+packet.src.proto_ipv4)
+
+        if (packet.arp.src_proto_ipv4=="192.168.2.2"and packet.arp.src_hw_mac !="52:54:00:12:35:00"):
+            print("Arp poison at: " + packet.arp.src_hw_mac )
 
         """ try :
-            packet_info = {
-            'Source IP': packet.ip.src,
-            'Destination IP': packet.ip.dst,
-            'Protocol': packet.highest_layer,
+        packet_info = {
+        'Source IP': packet.ip.src,
+        'Destination IP': packet.ip.dst,
+        'Protocol': packet.highest_layer,
 
-            # Add more fields as per your requirement
+        # Add more fields as per your requirement
         }
         except:
             print("err")
             packet_info="err"
 
         print(packet_info)
-"""
-# Specify the interface to capture packets from
+        """
+    # Specify the interface to capture packets from
+
+
 interface = 'wi-fi'
 
 # Start printing live packets
-print_live_packets(interface)
+print_live_packets(interface, "13:^11:13.!+", "192.168.3.1")
